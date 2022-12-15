2a7e93f33476e268c000e20b2fdce03270bcbe46
kartik <kgalewar@gmail.com>
2022-12-15 22:20:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
+import { useMemo, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [countOne,setCountOne] = useState(0);
+  const [countTwo,setCountTwo] = useState(0);
+
+  const incrementOne = ()=>{
+    setCountOne(countOne + 1)
+  }
+
+  const incrementTwo = ()=>{
+    setCountTwo(countTwo + 1)
+  }
+
+  const isEven =  useMemo(() => {
+    console.warn(".....")
+    let i =0;
+    while(i<200000000) i++;
+    return countOne%2 === 0
+  },[countOne])
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={incrementOne} >Count One - {countOne}</button>
+      {isEven ? "Even" : "Odd"}
+      <button onClick={incrementTwo} >Count Two - {countTwo}</button>
     </div>
   );
 }
